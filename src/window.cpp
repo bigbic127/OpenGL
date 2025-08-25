c70daa84074edc1f2bcf594d1626ff95087e384a
bigbic127 <bigbic127@gmail.com>
2025-08-26 08:10:01 +0900

    삼각형 그리기

--- src/window.cpp
+++ src/window.cpp
@@ -6,7 +6,7 @@ const GLint HEIGHT = 600;
 
 const GLchar* vertexShaderSource = R"(
     #version 450 core
-    int vec4 vertexPos;
+    layout (location =0) in vec4 vertexPos;
     void main()
     {
         gl_Position = vertexPos;
@@ -15,10 +15,10 @@ const GLchar* vertexShaderSource = R"(
 
 const GLchar* fragmentShaderSource = R"(
     #version 450 core
-    out vec4 fragColor;
+    out vec4 FragColor;
     void main()
     {
-        fragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+        FragColor = vec4(0.2f, 0.4f, 0.6f, 1.0f);
     }
 )";
 
@@ -56,13 +56,52 @@ int main()
         return -1;
     }
 
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //
+    unsigned int vertexShader, fragmentShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram); 
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     while(!glfwWindowShouldClose(window))
     {
         glClearColor(0.5f,0.5f,0.5f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
     glfwTerminate();
     glfwDestroyWindow(window);
     return 0;
