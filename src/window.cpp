58c1f3ac64f0ba3ceb22bb2e115acc70f84b3991
bigbic127 <bigbic127@gmail.com>
2025-08-31 10:18:29 +0900

    라이트 추가

--- src/window.cpp
+++ src/window.cpp
@@ -15,6 +15,8 @@ void Error_Callback(int error, const char* description);
 void FrameBufferSize_Callback(GLFWwindow* window, int width, int height);
 void Error_ShaderCompile(int shaderID, std::string type);
 
+glm::vec3 LIGHT_POSTION = glm::vec3(0.0f, 0.0f, -1.5f);
+
 float cubeVertices[] = {
     // Front face
     -1.0f, -1.0f,  1.0f,   0.0f,  0.0f,  1.0f,   0.0f, 0.0f, // Bottom-left
@@ -68,6 +70,27 @@ unsigned int cubeIndices[] = {
    20,21,22, 22,23,20
 };
 
+const char* lightVertexShaderSource = R"(
+    #version 410 core
+    layout (location = 0) in vec3 vPos;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    void main()
+    {
+        gl_Position = projection * view * model * vec4(vPos.xyz, 1.0f);
+    }
+)";
+
+const char* lightFragmentShaderSource = R"(
+    #version 410 core
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(1.0f);
+    }
+)";
+
 const char* vertexShaderSource = R"(
     #version 410 core
     layout (location = 0) in vec3 vPos;
@@ -78,23 +101,37 @@ const char* vertexShaderSource = R"(
     uniform mat4 projection;
     out vec2 TexCoord;
     out vec3 Normal;
+    out vec3 Model;
     void main()
     {
         gl_Position = projection * view * model * vec4(vPos.xyz, 1.0f);
+        Normal = mat3(transpose(inverse(model))) * vNor;
         TexCoord = vUVs;
-        Normal = vNor;
+        Model = vec3(model * vec4(vPos.xyz, 1.0f));
+
     }
 )";
 
 const GLchar* fragmentShaderSource = R"(
     #version 410 core
-    out vec4 fragColor;
+    out vec4 FragColor;
     in vec2 TexCoord;
     in vec3 Normal;
+    in vec3 Model;
+    uniform vec3 lightPos;
+    uniform vec3 viewPos;
+    uniform vec3 lightColor;
+    uniform vec3 objectColor = vec3(1.0, 1.0, 1.0);;
     uniform sampler2D texture1;
     void main()
     {
-        fragColor = texture(texture1, TexCoord);
+        vec3 ambient = 0.2 * lightColor;
+        vec3 normal = normalize(Normal);
+        vec3 lightDirection = normalize(lightPos-Model);
+        float value  = max(dot(normal,lightDirection), 0.0f);
+        vec3 diffuse = value * lightColor;
+        vec3 result = (ambient + diffuse) * objectColor;
+        FragColor = vec4(result * vec3(texture(texture1, TexCoord).xyz), 1.0f);
     }
 )";
 
@@ -130,22 +167,50 @@ int main()
 
     unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)(6*sizeof(float)));
     glEnableVertexAttribArray(2);
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); // VAO를 스테이트머신에서 초기화하여 추가작업은 VAO가 bind 되지 않는다.
 
-    glGenBuffers(1, &EBO);
+    unsigned int lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO); // lightVAO가 스테이트머신에 초기화, 추가작업은 lightVAO에 bind 된다.
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0); // VAO 초기화
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // VBO 초기화
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // EBO 초기화
+
+
+    unsigned int lightVertexShader, lightFragmentShader, lightShaderProgram;
+    lightVertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(lightVertexShader, 1, &lightVertexShaderSource, nullptr);
+    glCompileShader(lightVertexShader);
+    Error_ShaderCompile(lightVertexShader, "VERTEX");
+    lightFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(lightFragmentShader, 1, &lightFragmentShaderSource, nullptr);
+    glCompileShader(lightFragmentShader);
+    Error_ShaderCompile(lightFragmentShader, "FRAGMENT");
+    lightShaderProgram = glCreateProgram();
+    glAttachShader(lightShaderProgram, lightVertexShader);
+    glAttachShader(lightShaderProgram, lightFragmentShader);
+    glLinkProgram(lightShaderProgram);
+    Error_ShaderCompile(lightShaderProgram, "PROGRAM");
+    glUseProgram(lightShaderProgram);
+    glDeleteShader(lightVertexShader);
+    glDeleteShader(lightFragmentShader);
 
     unsigned int vertexShader, fragmentShader, shaderProgram;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -188,6 +253,7 @@ int main()
     textureShaderID = glGetUniformLocation(shaderProgram, "texture1");
     glUniform1i(textureShaderID, 0);
     glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureID);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -203,15 +269,27 @@ int main()
         model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0, 1.0, 0.0));
         model = glm::scale(model, glm::vec3(0.5f));
         view = glm::lookAt(glm::vec3(0.0f,1.0f,-2.0f), glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
-        projection = glm::perspective((float)70.0f, (float)WIDTH/HEIGHT, (float)0.1f, (float)1000.0f);
+        projection = glm::perspective(glm::radians(70.0f), (float)WIDTH/HEIGHT, 0.1f, 1000.0f);
         glUseProgram(shaderProgram);
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-
+        glUniform3fv(glGetUniformLocation(shaderProgram, "lightPos"), 1, &LIGHT_POSTION[0]);
+        glUniform3f(glGetUniformLocation(shaderProgram, "lightColor"),1.0f,1.0f,1.0f);
         glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, sizeof(cubeIndices)/sizeof(unsigned int), GL_UNSIGNED_INT, cubeIndices);
+        glDrawElements(GL_TRIANGLES, sizeof(cubeIndices)/sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+
+        glm::mat4 lightModel(1.0f);
+        lightModel = glm::translate(lightModel, LIGHT_POSTION);
+        lightModel = glm::scale(lightModel, glm::vec3(0.05f));
+        glUseProgram(lightShaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(lightShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+        glUniformMatrix4fv(glGetUniformLocation(lightShaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(lightShaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glBindVertexArray(lightVAO);
+        glDrawElements(GL_TRIANGLES, sizeof(cubeIndices)/sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
     }
     glDeleteBuffers(1, &VBO);
